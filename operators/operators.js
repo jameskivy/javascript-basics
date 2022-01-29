b4772676cd6cb1e8ba737fa0fce0f810902301bc
James Ivy <jameskivy@gmail.com>
2022-01-29 01:28:16 -0600

    done

--- operators/operators.js
+++ operators/operators.js
@@ -144,12 +144,12 @@
 
 // Operators Precedence
 
-let x = 2 + 3 * 4
-console.log(x);
-//14
+// let x = 2 + 3 * 4
+// console.log(x);
+// //14
 
-let y = (2 + 3) * 4
-console.log(y);
+// let y = (2 + 3) * 4
+// console.log(y);
 //20
 
 
