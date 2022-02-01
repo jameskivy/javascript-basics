7c39342e19ad0d00df1296794a002c7b838d4b86
James Ivy <jameskivy@gmail.com>
2022-02-01 00:25:48 -0600

    excercises

--- control-flow/controlFlow.js
+++ control-flow/controlFlow.js
@@ -187,12 +187,43 @@
 //     return (a > b) ? a : b;
 // }
 
-let landscape = isLandscape(400, 200)
-console.log(landscape);
+// Write a function that returns true if picture is landscape. If portrait return false.
 
-function isLandscape(width, height) {
-    return width > height ? width : height;
-}
+// console.log(isLandscape(1000, 600));
+
+// function isLandscape(width, height) {
+//     return (width > height);
+// }
+
+
+
+// Divisible by 3 => Fizz
+// Divisible by 5 => Buzz
+// Divisible by both 3 and 5 => FizzBuzz
+// Not divisible by 3 or 5 => input
+// Not a number => 'Not a number'
+
+
+// const output = fizzBuzz(false);
+// console.log(output);
+
+// function fizzBuzz(input) {
+//     if (typeof input !== 'number')
+//         return NaN;
+    
+//     if ((input % 3 === 0) && (input % 5 === 0))
+//         return 'FizzBuzz';
+
+//     if (input % 3 === 0)
+//         return 'Fizz';
+
+//     if (input % 5 === 0)
+//         return 'Buzz';
+
+//     return input;
+// }
+
+
 
 
 
