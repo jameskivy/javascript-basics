f8ce92e51b9e32d5e1a9e342552e410cc7636eb2
James Ivy <jameskivy@gmail.com>
2022-02-01 20:22:01 -0600

    updates

--- control-flow/controlFlow.js
+++ control-flow/controlFlow.js
@@ -224,6 +224,28 @@
 // }
 
 
+// Speed Limit = 70
+// 5 > 1 point
+// Math.floor(1.3)
+// 12 points -> suspended
+
+checkSpeed(130);
+
+function checkSpeed(speed) {
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+
+    if (speed < speedLimit + kmPerPoint)
+        console.log('Ok');
+    else {
+        let points = Math.floor((speed - speedLimit) / kmPerPoint);
+        if (points >= 12)
+            console.log('License suspended');
+        else
+        console.log('Points', points);
+    }
+}
+
 
 
 
