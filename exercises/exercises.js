88599299b5dddab2fb9e8d6a024a7be71e2569e7
James Ivy <jameskivy@gmail.com>
2022-02-19 04:08:02 -0600

    late night practice

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -32,16 +32,16 @@
 // function fizzBuzz(input) {
 //     if (typeof input !== 'number')
 //     return NaN;
-    
+
 //     if ((input % 3 === 0) && (input % 5 === 0))
 //     return 'FizzBuzz';
-    
+
 //     if (input % 3 === 0) 
 //     return 'Fizz';
-    
+
 //     if (input % 5 === 0)
 //     return 'Buzz';
-    
+
 //     return input;
 // }
 
@@ -122,6 +122,43 @@
 //         }
 
 //     return sum;
+// }
+
+
+
+////////// Exercise # 9 : Grade of Students ////////////
+
+// const marks = [80, 80, 50];
+
+// Average = 70
+
+// 1-59: F
+// 60-69: D
+// 70-79: C
+// 80-89: B
+// 90-100: A
+
+// console.log(calculateGrade(marks))
+
+// function calculateGrade(marks) {
+//     const average = calculateAverage(marks);
+//     if (average < 60) 
+//         return 'F';
+//     if (average < 70) 
+//         return 'D';
+//     if (average < 80) 
+//         return 'C';
+//     if (average < 90) 
+//         return 'B';
+//     return 'A';
+// }
+
+// function calculateAverage(array) {
+//     let sum = 0;
+
+//     for(let value of array)
+//         sum += value;
+//     return sum / array.length;
 // }
 
 
