2e74f5511f2a62137b315d181c98e01305eeab2d
James Ivy <jameskivy@gmail.com>
2022-02-19 08:33:36 -0600

    udpates

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -128,7 +128,7 @@
 
 ////////// Exercise # 9 : Grade of Students ////////////
 
-// const marks = [80, 80, 50];
+const marks = [80, 80, 50];
 
 // Average = 70
 
@@ -138,27 +138,27 @@
 // 80-89: B
 // 90-100: A
 
-// console.log(calculateGrade(marks))
+console.log(calculateGrade(marks))
 
-// function calculateGrade(marks) {
-//     const average = calculateAverage(marks);
-//     if (average < 60) 
-//         return 'F';
-//     if (average < 70) 
-//         return 'D';
-//     if (average < 80) 
-//         return 'C';
-//     if (average < 90) 
-//         return 'B';
-//     return 'A';
-// }
+function calculateGrade(marks) {
+    const average = calculateAverage(marks);
+    if (average < 60) 
+        return 'F';
+    else if (average < 70) 
+        return 'D';
+    else if (average < 80) 
+        return 'C';
+    else if (average < 90) 
+        return 'B'; 
+    return 'A';
+}
 
-// function calculateAverage(array) {
-//     let sum = 0;
+function calculateAverage(array) {
+    let sum = 0;
 
-//     for(let value of array)
-//         sum += value;
-//     return sum / array.length;
-// }
+    for(let value of array)
+        sum += value;
+    return sum / array.length;
+}
 
 
