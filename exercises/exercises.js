bb753b236228673082060aa34d5676f0029e6dbc
James Ivy <jameskivy@gmail.com>
2022-02-19 09:21:28 -0600

    ready for next section

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -128,7 +128,7 @@
 
 ////////// Exercise # 9 : Grade of Students ////////////
 
-const marks = [80, 80, 50];
+// const marks = [80, 80, 50];
 
 // Average = 70
 
@@ -138,27 +138,69 @@ const marks = [80, 80, 50];
 // 80-89: B
 // 90-100: A
 
-console.log(calculateGrade(marks))
+// console.log(calculateGrade(marks))
 
-function calculateGrade(marks) {
-    const average = calculateAverage(marks);
-    if (average < 60) 
-        return 'F';
-    else if (average < 70) 
-        return 'D';
-    else if (average < 80) 
-        return 'C';
-    else if (average < 90) 
-        return 'B'; 
-    return 'A';
-}
+// function calculateGrade(marks) {
+//     const average = calculateAverage(marks);
+//     if (average < 60) 
+//         return 'F';
+//     else if (average < 70) 
+//         return 'D';
+//     else if (average < 80) 
+//         return 'C';
+//     else if (average < 90) 
+//         return 'B'; 
+//     return 'A';
+// }
 
-function calculateAverage(array) {
-    let sum = 0;
+// function calculateAverage(array) {
+//     let sum = 0;
 
-    for(let value of array)
-        sum += value;
-    return sum / array.length;
-}
+//     for(let value of array)
+//         sum += value;
+//     return sum / array.length;
+// }
 
 
+
+////////// Exercise # 10 : Stars ////////////
+
+// showStars(10)
+
+// function showStars(rows) {
+//     for (let row = 1; row <= rows; row++) {
+//         let pattern = '';
+//         for (let i = 0; i < row; i++)
+//             pattern += '*';
+//         console.log(pattern);
+//     }
+// }
+
+
+
+////////// Exercise # 10 : PrimeNumbers ////////////
+
+
+// Prime (whose factors are only 1 and itself)
+// Composite 
+
+// 12 = 1, 2, 3, 4, 6, 12
+// Can be divided evenly by its factors
+
+// 11 = 1, 11
+// 13 = 1, 13
+
+// showPrimes(15);
+
+// function showPrimes(limit) {
+//     for (let number = 2; number <= limit; number++)
+//         if (isPrime(number)) console.log(number);
+// }
+
+// function isPrime(number) {
+//     for (let factor = 2; factor < number; factor++)
+//         if (number % factor === 0) 
+//             return false;
+
+//     return true;
+// }
