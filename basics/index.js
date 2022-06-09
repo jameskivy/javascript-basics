fccd9a70dbf26f8c839bf5d4162f5a299c34eec6
James Ivy <jameskivy@gmail.com>
2022-06-08 19:45:52 -0500

    refresher

--- basics/index.js
+++ basics/index.js
@@ -1,5 +1,5 @@
-// let name = 'James';
-// console.log(name);
+let name = 'James';
+console.log(name);
 
 // // variables cannot be a reserved keyword let, if, else, var
 // // should be meaningful
@@ -7,32 +7,34 @@
 // // Cannot contain a space of hyphen (-)
 // // Are case-sensitive
 
-// let firstName = 'James';
-// console.log(firstName);
-// let lastName = 'Ivy';
-// console.log(lastName);
+let firstName = 'James';
+console.log(firstName);
+let lastName = 'Ivy';
+console.log(lastName);
 
 // // var is rarely used anymore more popular is es5
 // // if you need to reassign a variable use let
 // // if you do not need to reassign a variable use const bc const variables cannot be reassigned 
 
-// const interestRate = 0.3;
-// interestRate = 1    // error in console bc const will not let you reassign have to use let
-// console.log(interestRate);
+let interestRate = 0.3;
+interestRate = 1    // error in console bc const will not let you reassign have to use let
+console.log(interestRate);
+
 
 // // Primitive / Value Types / Reference Types
 // // String, numbers, boolean, undefined, and null
 
-// let nameOne = 'James'; // string literal
-// console.log(typeof(nameOne))
-// let ageOne = 30; // number literal 
-// console.log(typeof(ageOne))
-// let isApproved = true // boolean literal - can be true or false
-// console.log(typeof(isApproved))
-// let firstNameOne = undefined; // if we do not initialize undefined will be returned - value must be assigned
-// console.log(typeof(firstNameOne))
-// let selectedColor = null; // use null to clear value of variable (ex: 'red')
-// console.log((typeof(selectedColor))) // type of is an object
+let nameOne = 'James'; // string literal
+console.log(typeof(nameOne))
+let ageOne = 30; // number literal 
+console.log(typeof(ageOne))
+let isApproved = true // boolean literal - can be true or false
+console.log(typeof(isApproved))
+let firstNameOne = undefined; // if we do not initialize undefined will be returned - value must be assigned
+console.log(typeof(firstNameOne))
+let selectedColor = null; // use null to clear value of variable (ex: 'red')
+console.log((typeof(selectedColor))) // type of is an object
+
 
 // JavaScript is a dynamic language
 
@@ -42,16 +44,17 @@
 // //Objects - name , age, address = properties of a person
 
 // key value pairs 
-// let person = {
-//     name: 'James',
-//     age: 27
-// };
+let person = {
+    name: 'James',
+    age: 27
+};
 
-// console.log(person)
+console.log(person)
 
 // // Dot Notation - cleaner and easier
-// person.name = 'John';
-// console.log(person.name)
+person.name = 'John';
+console.log(person.name)
+console.log(person.age)
 
 
 // // Bracket Notation
@@ -68,12 +71,12 @@
 // length is a property = returns number of items or elements in an array
 // an array is an data structure we use to represent a list of items
 
-// let selectedColors = ['red', 'blue'];
-// selectedColors[2] = 'green'
-// console.log(selectedColors);
-// selectedColors[3] = 28;
-// console.log(selectedColors)
-// console.log(selectedColors.length);
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = 'green';
+console.log(selectedColors);
+selectedColors[3] = 28;
+console.log(selectedColors)
+console.log(selectedColors.length);
 
 
 // //Functions - a set of statements that performs a task or calculates a value
@@ -83,11 +86,12 @@
 
 // this function is performing a task - to display something on the console
 
-// function greet(name, lastName) { // name is a parameter - time of declaration
-//     console.log('Hello ' + name + ' ' + lastName);
-// }
-// greet('John', 'Smith'); // argument to the greet function - value we supply for that parameter
-// greet('Mary', 'Smith')
+function greet(name, lastName) { // name is a parameter - time of declaration
+    console.log('Hello ' + name + ' ' + lastName);
+}
+greet('John', 'Smith'); // argument to the greet function - value we supply for that parameter
+greet('Mary', 'Smith')
+
 
 // Function that calculating a value
 
--- functions/functions.js
+++ functions/functions.js
@@ -1,22 +1,91 @@
-// Function Declarations vs Expression
+////////// Function Declarations vs Expression ///////////////////
 
+
+// Function declaration
 // there are two ways to define a function 
+// we do not have to terminate function declaration with ; - no errors
 
-// Function declaration 
+// walk();
 
-walk();
-
-function walk() {
-    console.log('walk');
-}
+// function walk() {
+//     console.log('walk');
+// }
 
 
 // Anonymous Function Expression
-let run = function () {
-    console.log('run');
-};
+// we can set a variable to a function
+// this function does not have a name
 
-let move = run;
-run();
-move();
+// let run = function () {
+//     console.log('run');
+// };
+
+// let move = run;  // move and run are referencing the same anonymous function which is one object in memory
+// run();
+// move();
+
+
+// Named Function Expression
+
+// let running = function walk() {
+//     console.log('run');
+// };
+
+
+
+//////  Hoisting //////////////
+// move function declaration to the top
+// hoisting is the process of moving function declarations to the top of the javascript file - done automatically
+
+// f declaration
+// we can call walk before it is defined
+
+// walk();
+
+// function walk() {
+//     console.log('walk');
+// }
+
+// f expression
+// cannot call run before it is initialized
+
+
+// const run = function() {
+//     console.log('run')
+// };
+
+// run();
+
+
+///////// Arguments ///////////
+// working with the arguments object
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// console.log(sum(1, 3));
+
+
+// function add() {
+//     let total = 0;
+//     for (let value of arguments)
+//         total += value;
+//     return total;
+// }
+
+// console.log(add(1, 2, 3, 4, 5, 100));
+
+
+
+/////// Rest Operator ////
+
+// function add(...args) {
+//     return args.reduce((a, b) => a * b);
+// }
+
+// console.log(add(1, 2, 3, 4, 5, 100));
+
+console.log(3>2>1)
+
 
